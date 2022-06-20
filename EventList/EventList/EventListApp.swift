8899dcc198ffd642ef1711944d8e8e85547c3421
Eslam Shaker <binshakerr@gmail.com>
2022-06-20 23:21:43 +0400

    project start

---
+++ EventList/EventList/EventListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventListApp.swift
+//  EventList
+//
+//  Created by Eslam Shaker on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct EventListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
