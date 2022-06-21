cf5a6bc994dacf3069da0ef881c7626cf2e264e2
Eslam Shaker <binshakerr@gmail.com>
2022-06-21 23:15:03 +0400

    added json reader

---
+++ EventList/EventList/JsonReader.swift
@@ -0,0 +1,28 @@
+//
+//  JsonReader.swift
+//  EventList
+//
+//  Created by Eslam Shaker on 21/06/2022.
+//
+
+import Foundation
+
+class JsonReader {
+    
+    enum IOError: Error {
+        case pathNotFound
+    }
+
+    static let shared = JsonReader()
+    private init() {}
+    private let bundle = Bundle.main
+    
+    func load<T: Decodable>(fileName: String, type: T.Type) throws -> T {
+        guard let path = bundle.url(forResource: fileName, withExtension: "json") else {
+            throw IOError.pathNotFound
+        }
+        let data = try Data(contentsOf: path)
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    }
+}
